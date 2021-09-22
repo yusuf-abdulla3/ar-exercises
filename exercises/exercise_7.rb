532fce4ba30859a547c08a2cf364cdc335601527
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-09-22 13:13:15 +0000

    Completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,30 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than: 40, less_than: 200}
+  validates :store, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, minimum: 0}
+  validate :check_has_apparel
+  
+  def check_has_apparel 
+    mens_apparel || womens_apparel 
+  end
+
+end
+
+puts "Enter store name here: "
+store_name = $stdin.gets.chomp
+new_store = Store.create(name: store_name)
+
+puts new_store.errors.full_messages
+
+
+
