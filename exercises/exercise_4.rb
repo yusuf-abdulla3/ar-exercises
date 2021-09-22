11242284e859b8eb50570b69043d0c1bc46342c4
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-09-22 12:51:44 +0000

    Completed exercises 4 & 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,21 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end 
+
+@womens_stores = Store.where('annual_revenue < 1000000', womens_apparel:true)
+
+@womens_stores.each do |store|
+  puts "Store Name: #{store.name} Annual Revenue: #{store.annual_revenue}"
+end 
+
+
+
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+
+@average = Store.average(:annual_revenue)
+puts @total_revenue
+
+@stores_greater_than_a_million = Store.where('annual_revenue >= 1000000').count
+puts @stores_greater_than_a_million
+
