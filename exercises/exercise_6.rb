04300743149e29ad409d36af31144d2dbe29eb23
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-09-22 12:59:55 +0000

    Completed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+puts Store.has_many(:employees)
+puts Store.belongs_to(:store)
+
+@store1.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 34)
+@store2.employees.create(first_name: "Yusuf", last_name: "Abdulla", hourly_rate: 65)
+@store2.employees.create(first_name: "Kyle", last_name: "Lowry", hourly_rate: 100)
+@store2.employees.create(first_name: "DeMar", last_name: "DeRozan", hourly_rate: 100)
